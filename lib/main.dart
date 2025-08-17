de19015e285f21e7ee104e4b91a530b938d2eb09
Phisan Sookkhee <phisan.shukkhi@gmail.com>
2025-08-17 19:27:56 +0700

    update README

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,11 @@ import 'routes/app_routes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   // Initialize Hive
   final directory = await getApplicationDocumentsDirectory();
   Hive.init(directory.path);
-  
+
   runApp(const MainApp());
 }
 
